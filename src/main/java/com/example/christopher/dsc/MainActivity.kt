cdbecbcab088c6c261e164e01d13ba382d9c44fa
ChristopherHartanto <38125624+ChristopherHartanto@users.noreply.github.com>
2018-11-28 22:55:02 +0700

    Add files via upload

--- src/main/java/com/example/christopher/dsc/MainActivity.kt
+++ src/main/java/com/example/christopher/dsc/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.christopher.dsc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.christopher.dsc.R.id.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                Home ->{
+                }
+                BukaMall ->{
+                }
+                Keranjang -> {
+                }
+                Transaksi ->{
+                }
+                Akun -> {
+                }
+            }
+            true
+        }
+        bottom_navigation.selectedItemId = Home
     }
 }
